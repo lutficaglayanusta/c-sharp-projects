d194ab0570a8e1bd8a44afa9ab5238338fed3443
lutficaglayanusta <65968577+lutficaglayanusta@users.noreply.github.com>
2024-10-02 16:38:01 +0300

    kodluyoruz

---
+++ üçgen çizme/Program.cs
@@ -0,0 +1,26 @@
+﻿namespace üçgen_çizme;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Triangle triangle = new Triangle();
+        triangle.drawTriangle();
+        
+    }
+    class Triangle{
+        public int number;
+        public Triangle(){
+            Console.Write("Lütfen üçgenin boyutunu belirleyiniz:");
+            this.number =Convert.ToInt32(Console.ReadLine());
+        }
+        public void drawTriangle(){
+            for(int i=1;i<=number;i++){
+                for(int j=0;j<i;j++){
+                    Console.Write("*");
+                }
+                Console.WriteLine("");
+            }
+        }
+    }
+}
